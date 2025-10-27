b47a46a8dd0096c0572375654501a23a79aca7e9
mrHeadlessBoy <mrheadlessboy@gmail.com>
2025-10-27 11:08:36 +0700

    yeah I've find what make cannot connect

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 import studentRoutes from './modules/user/students.routes';
 
 const app = express();
+
 app.use(express.json());
+
+app.use(cors({ origin: "http://localhost:5173" }));
 
 
 app.use('/v1/students', studentRoutes);
