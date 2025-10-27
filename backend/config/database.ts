75924b896361b71a06a3bc5a47bcb02e907293ac
mrHeadlessBoy <mrheadlessboy@gmail.com>
2025-10-27 10:49:24 +0700

    change and connect database to render

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,11 +1,14 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "students",
-    password: "nohe@d4pg?",
+    user: "postgress",
+    host: "dpg-d3vef5je5dus73aaov0g-a.singapore-postgres.render.com",
+    database: "students_o2g2",
+    password: "xfVx6E8MR3CNKM6Sqn8DEEGQATZOWAqt",
     port: 5432, 
+    ssl: { rejectUnauthorized: false }
 })
 
 export default pool;
+//dpg-d3vef5je5dus73aaov0g-a.singapore-postgres.render.com
+//postgresql://postgress:xfVx6E8MR3CNKM6Sqn8DEEGQATZOWAqt@dpg-d3vef5je5dus73aaov0g-a.singapore-postgres.render.com/students_o2g2
