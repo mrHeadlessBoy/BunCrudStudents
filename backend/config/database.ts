201b683f318c94dd8af3505fec0a1e6ff9f5415d
mrHeadlessBoy <mrheadlessboy@gmail.com>
2025-12-03 02:08:47 +0700

    Update database.ts

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,11 +1,8 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-    user: "postgres",
-    host: "ep-lucky-truth-a1i3xp0e-pooler.ap-southeast-1.aws.neon.tech",
-    database: "students",
-    password: "npg_vTO16BEILdnz",
-    port: 5432,
+    connectionString:
+        "postgresql://neondb_owner:npg_vTO16BEILdnz@ep-lucky-truth-a1i3xp0e-pooler.ap-southeast-1.aws.neon.tech/students?sslmode=require",
     ssl: {
         rejectUnauthorized: false
     }
