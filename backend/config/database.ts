200378d18d13f54acd8289dd8f5eeddd98790043
mrHeadlessBoy <mrheadlessboy@gmail.com>
2025-12-03 02:01:50 +0700

    เปลี่ยน database ใช้ neon แทน render และ railway ที่หิวตัง

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -1,14 +1,14 @@
 import { Pool } from "pg";
 
 const pool = new Pool({
-    user: "postgress",
-    host: "dpg-d3vef5je5dus73aaov0g-a.singapore-postgres.render.com",
-    database: "students_o2g2",
-    password: "xfVx6E8MR3CNKM6Sqn8DEEGQATZOWAqt",
-    port: 5432, 
-    ssl: { rejectUnauthorized: false }
-})
+    user: "neondb_owner",
+    host: "ep-lucky-truth-a1i3xp0e-pooler.ap-southeast-1.aws.neon.tech",
+    database: "students",
+    password: "npg_vTO16BEILdnz",
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 export default pool;
-//dpg-d3vef5je5dus73aaov0g-a.singapore-postgres.render.com
-//postgresql://postgress:xfVx6E8MR3CNKM6Sqn8DEEGQATZOWAqt@dpg-d3vef5je5dus73aaov0g-a.singapore-postgres.render.com/students_o2g2
